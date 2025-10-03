ca1d414205e400fe4630ddbb0b715be23bf0cb6b
Renan Souza <renan.souza@powerpush.com.br>
2025-10-03 05:35:29 -0300

    on development

---
+++ Source/ProjectCatch/Private/Controllers/DSCatchPlayerController.cpp
@@ -0,0 +1,3 @@
+// Made by Dallai Studios - 2025
+
+#include "Controllers/DSCatchPlayerController.h"
---
+++ Source/ProjectCatch/Public/Controllers/DSCatchPlayerController.h
@@ -0,0 +1,12 @@
+// Made by Dallai Studios - 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DSCatchPlayerController.generated.h"
+
+UCLASS()
+class PROJECTCATCH_API ADSCatchPlayerController : public APlayerController {
+	GENERATED_BODY()
+};
