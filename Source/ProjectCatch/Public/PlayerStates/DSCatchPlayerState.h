73d34a25bf6f03fe82bc743f32ba2224a620eddc
Renan Souza <renan.souza@powerpush.com.br>
2025-10-05 01:26:58 -0300

    on development

---
+++ Source/ProjectCatch/Public/PlayerStates/DSCatchPlayerState.h
@@ -0,0 +1,12 @@
+// Made by Dallai Studios - 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "DSCatchPlayerState.generated.h"
+
+UCLASS()
+class PROJECTCATCH_API ADSCatchPlayerState : public APlayerState {
+	GENERATED_BODY()
+};
